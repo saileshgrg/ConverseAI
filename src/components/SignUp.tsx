4f6b1ffb087dac23a0b90fde73b7c8c3763b3111
Sahas001 <tsahas2059@gmail.com>
2023-06-30 16:09:50 +0545

    SignIn page improved

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,54 +1,84 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
-export default function SignUp() {
+const Logo = require("../assets/chatbot.png");
+class SignUp extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      email: {
+        name: "email",
+        value: "",
+        required: true,
+        type: "email",
+        placeholder: "Email",
+      },
+      password: {
+        name: "password",
+        value: "",
+        required: true,
+        type: "password",
+        placeholder: "Password",
+      },
+    };
+  }
+  render() {
+    const { email, password } = this.state;
     return (
       <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
-              className="mx-auto h-20 w-auto"
-              src="https://cdn.technologyadvice.com/wp-content/uploads/2018/02/friendly-chatbot.jpg"
+              className="mx-auto h-12 w-auto"
+              src={Logo}
               alt="Your Company"
             />
+            <h3 className="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-indigo-600">
+              ConverseAI
+            </h3>
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign in to your account
             </h2>
           </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+          <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Email address
+                  </label>
+                </div>
                 <div className="mt-2">
                   <input
                     id="email"
-                    name="email"
-                    type="email"
+                    name={email.name}
+                    type={email.type}
                     autoComplete="email"
-                    required
+                    required={email.required}
                     className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder={email.placeholder}
                   />
                 </div>
               </div>
-  
+
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     Password
                   </label>
                   <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    <a
+                      href="#"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
                       Forgot password?
                     </a>
                   </div>
@@ -56,15 +86,16 @@ export default function SignUp() {
                 <div className="mt-2">
                   <input
                     id="password"
-                    name="password"
-                    type="password"
+                    name={password.name}
+                    type={password.type}
                     autoComplete="current-password"
-                    required
+                    required={password.required}
                     className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder={password.placeholder}
                   />
                 </div>
               </div>
-  
+
               <div>
                 <button
                   type="submit"
@@ -74,16 +105,21 @@ export default function SignUp() {
                 </button>
               </div>
             </form>
-  
+
             <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
+              Don't have an account?{" "}
+              <a
+                href="#"
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                Create One
               </a>
             </p>
           </div>
         </div>
       </>
-    )
+    );
   }
-  
+}
+
+export default SignUp;
